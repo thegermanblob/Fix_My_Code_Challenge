2a73a15e0749283fc29d7046f40e8c08857a301a
Thegermanblob <thegermanblob@gmail.com>
2021-09-03 17:42:42 +0000

    Add directory

---
+++ 0x00-challange/2-sort.rb
@@ -0,0 +1,36 @@
+###
+#
+#  Sort integer arguments (ascending) 
+#
+###
+
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+    
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            if i == 0
+              result.insert(0, i_arg)
+            else
+              result.insert(i, i_arg)
+
+            end
+            is_inserted = true
+        break
+        end
+    end
+    result << i_arg if !is_inserted
+end
+
+puts result
